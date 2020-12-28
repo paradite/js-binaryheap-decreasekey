d154ce91b1dfbfbd792155d14a4acd453616f257
Zhu Liang <zhuliangg11@gmail.com>
2020-12-28 15:54:53 +0800

    test(decreasekey): add test for decreaseKey

--- binaryheap-decreasekey.test.js
+++ binaryheap-decreasekey.test.js
@@ -71,3 +71,41 @@ test('max heap', () => {
   expect(queue.pop()).toBe(objs[3]);
   expect(queue.pop()).toBe(undefined);
 });
+
+test('max heap decrease key', () => {
+  const queue = new BinaryHeap(
+    (a) => -a.value,
+    (a) => {
+      return a.id;
+    },
+    'value'
+  );
+  let objs = [
+    {
+      id: 0,
+      value: 1,
+    },
+    {
+      id: 1,
+      value: 5,
+    },
+    {
+      id: 2,
+      value: 10,
+    },
+    {
+      id: 3,
+      value: -5,
+    },
+  ];
+  for (let i = 0; i < objs.length; i++) {
+    queue.push(objs[i]);
+  }
+  // increase value to make it appear in front
+  queue.decreaseKey(3, 99);
+  expect(queue.pop()).toBe(objs[3]);
+  expect(queue.pop()).toBe(objs[2]);
+  expect(queue.pop()).toBe(objs[1]);
+  expect(queue.pop()).toBe(objs[0]);
+  expect(queue.pop()).toBe(undefined);
+});
